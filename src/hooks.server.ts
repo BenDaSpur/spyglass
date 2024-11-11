49ad0aa45f1922caeeb7aca13552c5658b672cac
Ben Spurlock <spurlock114@gmail.com>
2024-11-10 22:04:53 -0800

    Comment out cron scheduling in hooks.server.ts

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,9 +2,9 @@ import * as scripty from '$lib/cron';
 import type { Handle } from '@sveltejs/kit';
 import cron from 'node-cron';
 
-cron.schedule('0 * * * *', () => {
-	scripty.run();
-});
+// cron.schedule('0 * * * *', () => {
+// 	scripty.run();
+// });
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const response = await resolve(event);
