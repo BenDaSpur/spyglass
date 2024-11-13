536a37edde252ea1d58fe21b5fe83d0d6ef1d50d
Ben Spurlock <spurlock114@gmail.com>
2024-11-13 08:01:56 -0800

    Update cron job setup in hooks server to import and run functions

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,10 @@
-// import { run } from '$lib/cron';
 import type { Handle } from '@sveltejs/kit';
-// import cron from 'node-cron';
+import run from './lib/cron';
+import cron from 'node-cron';
 
-// cron.schedule('0 * * * *', () => {
-// 	run();
-// });
+cron.schedule('0 * * * *', () => {
+	run();
+});
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const response = await resolve(event);
