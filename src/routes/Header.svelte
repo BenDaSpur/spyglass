14a9424bcd706ab4ab09a5f48b66edb30e000f21
Ben Spurlock <spurlock114@gmail.com>
2024-11-14 08:56:30 -0800

    Remove unnecessary navbar toggler and collapse components

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -30,24 +30,7 @@
 		<img src="/logo.svg" alt="Spyglass" width="30" height="30" class="d-inline-block align-top" />
 		Spyglass
 	</NavbarBrand>
-	<NavbarToggler on:click={() => (isOpen = !isOpen)} />
-	<Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
-		<Nav class="ms-auto" navbar>
-			<!-- <NavItem on:click={toggle}>
-				<NavLink>Why?</NavLink>
-			</NavItem> -->
-			<NavItem>
-				<NavLink href="https://github.com/BenDaSpur/spyglass" target="_blank">GitHub</NavLink>
-			</NavItem>
-			<!-- <Dropdown nav inNavbar>
-				<DropdownToggle nav caret>Options</DropdownToggle>
-				<DropdownMenu end>
-					<DropdownItem>Option 1</DropdownItem>
-					<DropdownItem>Option 2</DropdownItem>
-					<DropdownItem divider />
-					<DropdownItem>Reset</DropdownItem>
-				</DropdownMenu>
-			</Dropdown> -->
-		</Nav>
-	</Collapse>
+	<NavItem>
+		<NavLink href="https://github.com/BenDaSpur/spyglass" target="_blank">GitHub</NavLink>
+	</NavItem>
 </Navbar>
